52535b3fbb4238e2f597dee5c414e70b871916ba
SvetlanaDD <asweet.lana@gmail.com>
2022-09-14 01:38:41 +0300

    Error correction

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class MagicBox<T> {
     private int maxItem;
-    T[] items;
+    private T[] items;
 
     public MagicBox(int maxItem) {
         this.maxItem = maxItem;
@@ -20,28 +20,13 @@ public class MagicBox<T> {
     }
 
     public T pick() {
-        int numNull = 0;
-        try {
-            for (int i = 0; i < items.length; i++) {
-                if (items[i] == null) {
-                    numNull = i;
-                    items[items.length] = null;
-                }
-            }
-        } catch (RuntimeException exception) {
-            System.out.println("Коробка не полна, осталось еще " + (items.length - numNull) + " ячеек");
-        }
         Random random = new Random();
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка не полна, осталось еще " + (items.length - i) + " ячеек");
+            }
+        }
         int randomInt = random.nextInt(items.length);
         return items[randomInt];
-/*
- try {
-                productNumber = Integer.parseInt(parts[0]);
-                productCount = Integer.parseInt(parts[1]);
-            } catch (RuntimeException exception) {
-                System.out.println("Введен не корректный текст");
-                continue;
-            }
-* */
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException {
         MagicBox<String> stringMagicBox = new MagicBox<>(3);
         System.out.println("stringMagicBox.add() = " + stringMagicBox.add("One"));
         System.out.println("stringMagicBox.add() = " + stringMagicBox.add("Two"));
-        stringMagicBox.pick();
         System.out.println("stringMagicBox.add() = " + stringMagicBox.add("Three"));
         System.out.println("stringMagicBox.add() = " + stringMagicBox.add("Four"));
         System.out.println("Случайный элемент pick() = " + stringMagicBox.pick());
@@ -12,8 +11,5 @@ public class Main {
         System.out.println("intMagicBox.add() = " + intMagicBox.add(1));
         System.out.println("intMagicBox.add() = " + intMagicBox.add(2));
         intMagicBox.pick();
-        System.out.println("intMagicBox.add() = " + intMagicBox.add(3));
-        System.out.println("intMagicBox.add() = " + intMagicBox.add(4));
-        System.out.println("Случайный элемент pick() = " + intMagicBox.pick());
     }
 }
